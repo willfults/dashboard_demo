b4bbad1ff469ca87c6ed77a1baf1738906e82573
Will Fults <wfults@callsource.com>
2013-06-06 17:10:33 -0700

    initial Rails + gridster integration

---
+++ app/assets/javascripts/dashboard.js
@@ -0,0 +1,7 @@
+var gridster = "";
+$(document).ready(function () {
+    $(".gridster ul").gridster({
+        widget_margins: [10, 10],
+        widget_base_dimensions: [180, 180]
+    });
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DashboardDemo::Application.config.secret_token = 'd7f464a40c46a9e136e1782f994b570c369f98a231e756defa38e345ae7c39eb86da9da38cd9e4e2046ff26ffdce32b53cffe506e7270f11dcef404810b906d2'
