ad2c4fff1bbe64b16e9387b01200c24967a14b90
Will Fults <wfults@callsource.com>
2013-06-12 12:00:28 -0700

    used json to populate the graphs instead of hardcoding the data

--- app/assets/javascripts/widgets/graphs/piegraph.js
+++ app/assets/javascripts/widgets/graphs/piegraph.js
@@ -11,51 +11,50 @@ $(function () {
 		    };
 		});
 		
-		// Build the chart
-        $('#piechart').highcharts({
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false
-            },
-            title: {
-                text: 'CallSource sales by division'
-            },
-            tooltip: {
-        	    pointFormat: '{series.name}: <b>{point.percentage}%</b>',
-            	percentageDecimals: 1
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: true,
-                        color: '#000000',
-                        connectorColor: '#000000',
-                        formatter: function() {
-                            return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';
-                        }
-                    }
-                }
-            },
-            series: [{
-                type: 'pie',
-                name: 'Market share',
-                data: [
-                    ['Auto',   45.0],
-                    ['Home',       26.8],
-                    {
-                        name: 'Phone',
-                        y: 12.8,
-                        sliced: true,
-                        selected: true
-                    },
-                    ['IT',    8.5],
-                    ['Medical',     6.2],
-                    ['Other',   0.7]
-                ]
-            }]
-        });
+		$.getJSON('sample.json', function(data) {
+			$.each(data, function (i, point) {
+			    point.y = point.data;
+			});
+			// Build the chart
+	        $('#piechart').highcharts({
+	            chart: {
+	                plotBackgroundColor: null,
+	                plotBorderWidth: null,
+	                plotShadow: false,
+	                height: 340,
+	            },
+	            title: {
+	                text: 'CallSource sales by division'
+	            },
+	            tooltip: {
+	        	    pointFormat: '{series.name}: <b>{point.percentage}%</b>',
+	            	percentageDecimals: 1
+	            },
+	            plotOptions: {
+	                pie: {
+	                    allowPointSelect: true,
+	                    cursor: 'pointer',
+	                    dataLabels: {
+	                        enabled: true,
+	                        color: '#000000',
+	                        connectorColor: '#000000',
+	                        formatter: function() {
+	                            return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';
+	                        }
+	                    }
+	                }
+	            },
+	            series: [{
+	                type: 'pie',
+	                name: 'Market share',
+	                data: data
+	            }]
+	        });
+		});
+		
+
+				
+
     });
+    
     
--- app/assets/javascripts/widgets/graphs/spline.js
+++ app/assets/javascripts/widgets/graphs/spline.js
@@ -1,61 +1,60 @@
 $(function () {
-        $('#spline').highcharts({
-            chart: {
-                type: 'areaspline'
-            },
-            title: {
-                text: 'Sales last seven days'
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'left',
-                verticalAlign: 'top',
-                x: 150,
-                y: 100,
-                floating: true,
-                borderWidth: 1,
-                backgroundColor: '#FFFFFF'
-            },
-            xAxis: {
-                categories: [
-                    'Sunday',
-                    'Monday',
-                    'Tuesday',
-                    'Wednesday',
-                    'Thursday',
-                    'Friday',
-                    'Saturday'
-                ],
-                plotBands: [{ // visualize the weekend
-                    from: 4.5,
-                    to: 6.5,
-                    color: 'rgba(68, 170, 213, .2)'
-                }]
-            },
-            yAxis: {
-                title: {
-                    text: 'In thousands'
-                }
-            },
-            tooltip: {
-                shared: true,
-                valueSuffix: ' thousand'
-            },
-            credits: {
-                enabled: false
-            },
-            plotOptions: {
-                areaspline: {
-                    fillOpacity: 0.5
-                }
-            },
-            series: [{
-                name: 'John',
-                data: [3, 4, 3, 5, 4, 10, 0]
-            }, {
-                name: 'Jane',
-                data: [1, 3, 4, 3, 3, 5, 0]
-            }]
-        });
+
+        $.getJSON('spline.json', function(data) {
+        	$('#spline').highcharts({
+	            chart: {
+	                type: 'areaspline',
+	                height: 360,
+	            },
+	            title: {
+	                text: 'Sales last seven days'
+	            },
+	            legend: {
+	                layout: 'vertical',
+	                align: 'left',
+	                verticalAlign: 'top',
+	                x: 150,
+	                y: 100,
+	                floating: true,
+	                borderWidth: 1,
+	                backgroundColor: '#FFFFFF'
+	            },
+	            xAxis: {
+	                categories: [
+	                    'Sunday',
+	                    'Monday',
+	                    'Tuesday',
+	                    'Wednesday',
+	                    'Thursday',
+	                    'Friday',
+	                    'Saturday'
+	                ],
+	                plotBands: [{ // visualize the weekend
+	                    from: 4.5,
+	                    to: 6.5,
+	                    color: 'rgba(68, 170, 213, .2)'
+	                }]
+	            },
+	            yAxis: {
+	                title: {
+	                    text: 'In thousands'
+	                }
+	            },
+	            tooltip: {
+	                shared: true,
+	                valueSuffix: ' thousand'
+	            },
+	            credits: {
+	                enabled: false
+	            },
+	            plotOptions: {
+	                areaspline: {
+	                    fillOpacity: 0.5
+	                }
+	            },
+	            series: data
+	        });
+        }).error(function(data, data2, data3) { console.log("error" , data, data2, data3); });
+            
     });
     
